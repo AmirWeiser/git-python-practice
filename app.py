cb788ebb6d0b38aa06eb1d1f4b6f3671b85eac98
AmirWeiser <Amirweiser2@gmail.com>
2026-07-16 11:56:13 +0300

    slightly changed message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def greet(name):
     now = datetime.now()
-    return f"Hello, {name}, the time is: {now}!"
+    return f"Hello, {name}, the time is: {now}! Enjoy!"
 
 
 if __name__ == "__main__":
